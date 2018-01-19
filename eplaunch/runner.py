03812beca4e384ace8133d80dc1e82516b5fa447
Edwin Lee <leeed2001@gmail.com>
2018-01-19 14:16:31 -0600

    Major reorganization

---
+++ eplaunch/runner.py
@@ -0,0 +1,6 @@
+import gettext
+from eplaunch.interface.application import EpLaunchApplication
+
+gettext.install("app")  # replace with the appropriate catalog name
+app = EpLaunchApplication(0)
+app.MainLoop()
