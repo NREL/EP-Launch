79f46e4ac3ef99e613b8da33b637bd3cdab80d32
Edwin Lee <leeed2001@gmail.com>
2018-01-19 14:38:21 -0600

    Add setup.py with entrypoint

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='EPLaunch3',
+    version='0.1',
+    packages=['eplaunch', 'eplaunch.interface'],
+    url='https://energyplus.net',
+    license='ModifiedBSD',
+    author='DOE',
+    author_email='webmaster@doe.gov',
+    description='Graphical Interface and Workflow Manager for EnergyPlus',
+    scripts='eplaunch/runner.py',
+)
