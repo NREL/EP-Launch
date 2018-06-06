30a1ac646bd2712d38be9bc2f6c4be923c363b94
JasonGlazer <jglazer@gard.com>
2018-06-06 09:22:59 -0500

    Add file to force new directories to be present

--- eplaunch/tests/utilities/test_locateworkflows.py
+++ eplaunch/tests/utilities/test_locateworkflows.py
@@ -9,6 +9,7 @@ class TestLocateWorkflows(unittest.TestCase):
     def test_find(self):
         loc_wf = LocateWorkflows()
         directories = loc_wf.find()
+        print(directories)
         tests_utilties_energyplus_directory, workflow_folder = os.path.split(directories[0])
         self.assertEqual(workflow_folder, "workflows")
         tests_utilities_directory, energyplus_folder = os.path.split(tests_utilties_energyplus_directory)
