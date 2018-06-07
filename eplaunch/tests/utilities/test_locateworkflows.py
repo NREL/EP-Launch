7cb86cc8d32debebf951ca6b2a1d94ca0d6257f3
JasonGlazer <jglazer@gard.com>
2018-06-07 08:25:34 -0500

    Fix formatting and make unit test for locate workflows platform specific since it does not work on Travis

--- eplaunch/tests/utilities/test_locateworkflows.py
+++ eplaunch/tests/utilities/test_locateworkflows.py
@@ -16,7 +16,7 @@ class TestLocateWorkflows(unittest.TestCase):
             current_directory = '/Applications/'
         else:
             current_directory = '/usr/local/bin/'
-        self.energyplus_directory = os.path.join(current_directory,"EnergyPlusV5-9-0")
+        self.energyplus_directory = os.path.join(current_directory, "EnergyPlusV5-9-0")
         if not os.path.isdir(self.energyplus_directory):
             os.mkdir(self.energyplus_directory)
         self.workflow_directory = os.path.join(self.energyplus_directory, "workflows")
@@ -24,13 +24,14 @@ class TestLocateWorkflows(unittest.TestCase):
             os.mkdir(self.workflow_directory)
 
     def test_find(self):
-        loc_wf = LocateWorkflows()
-        directories = loc_wf.find()
-        directories59 = [dir for dir in directories if "5-9" in dir]
-        tests_utilties_energyplus_directory, workflow_folder = os.path.split(directories59[0])
-        self.assertEqual(workflow_folder, "workflows")
-        tests_utilities_directory, energyplus_folder = os.path.split(tests_utilties_energyplus_directory)
-        self.assertEqual(energyplus_folder, "EnergyPlusV5-9-0")
+        if Platform.get_current_platform() == Platform.WINDOWS:
+            loc_wf = LocateWorkflows()
+            directories = loc_wf.find()
+            directories59 = [dir for dir in directories if "5-9" in dir]
+            tests_utilties_energyplus_directory, workflow_folder = os.path.split(directories59[0])
+            self.assertEqual(workflow_folder, "workflows")
+            tests_utilities_directory, energyplus_folder = os.path.split(tests_utilties_energyplus_directory)
+            self.assertEqual(energyplus_folder, "EnergyPlusV5-9-0")
 
     def tearDown(self):
         os.rmdir(self.workflow_directory)
