1a05a3ff8e1c85662830e39a0202050daf4c920f
JasonGlazer <jglazer@gard.com>
2018-06-15 08:34:44 -0500

    Add handling for older energyplus versions. Still just testing functionality.

--- eplaunch/utilities/locateworkflows.py
+++ eplaunch/utilities/locateworkflows.py
@@ -36,18 +36,60 @@ class LocateWorkflows(object):
             energyplus_application = os.path.join(energyplus_directory, "energyplus.exe")
             if os.path.exists(energyplus_application):
                 self.list_of_energyplus_applications.append(energyplus_application)
-                self.get_specific_version(energyplus_application)
+                found, version_string, build_string = self.get_specific_version_from_exe(energyplus_application)
+                if not found:
+                    found, version_string, build_string = self.get_specific_version_from_idd(energyplus_directory)
         print(self.list_of_energyplus_applications)
 
-    def get_specific_version(self, path_to_energyplus_app):
-        completed = subprocess.run([path_to_energyplus_app, "--version"], stdout=subprocess.PIPE)
-        output_line = completed.stdout
-        output_line_string = output_line.decode("utf-8")
-        _, full_version_string = output_line_string.split(',')
-        full_version_string = full_version_string.strip()
-        three_number_version, sha_string = full_version_string.split('-')
-        _, three_number_version = three_number_version.split(' ')
+    def get_specific_version_from_exe(self, path_to_energyplus_app):
+        print("Getting version from exe: ", path_to_energyplus_app)
+        output_line_string = ""
+        try:
+            completed = subprocess.run([path_to_energyplus_app, "--version"], stdout=subprocess.PIPE, timeout=1)
+            output_line = completed.stdout
+            output_line_string = output_line.decode("utf-8")
+        except subprocess.TimeoutExpired:
+            print("timeout occurred")
+        print(output_line_string)
+        if ',' in output_line_string:
+            _, full_version_string = output_line_string.split(',')
+            full_version_string = full_version_string.strip()
+            three_number_version, sha_string = full_version_string.split('-')
+            _, three_number_version = three_number_version.split(' ')
+            print("==================")
+            print(three_number_version)
+            print(sha_string)
+            print("------------------")
+            return True, three_number_version, sha_string
+        else:
+            return False, "", ""
+
+    def get_specific_version_from_idd(self, path_to_energyplus_directory):
+        print("Getting version from idd in directory: ", path_to_energyplus_directory)
+        idd_loc = os.path.join(path_to_energyplus_directory, "Energy+.idd")
+        three_number_version = ""
+        sha_string = ""
+        if os.path.exists(idd_loc):
+            iddline = ""
+            buildline = ""
+            with open(idd_loc, "r") as file:
+                iddline = file.readline()
+                buildline = file.readline()
+            iddline = iddline.strip()
+            buildline = buildline.strip()
+            if " " in iddline:
+                idd_version_keyword, three_number_version = iddline.split(" ")
+                if idd_version_keyword != "!IDD_Version":
+                    three_number_version = ""
+            if " " in buildline:
+                idd_build_keyword, sha_string = buildline.split(" ")
+                if idd_build_keyword != "!IDD_BUILD":
+                    sha_string = ""
         print("==================")
         print(three_number_version)
         print(sha_string)
         print("------------------")
+        if three_number_version != "":
+            return True, three_number_version, sha_string
+        else:
+            return False, three_number_version, sha_string
