c30b908701f05dca35a46a891ac2b89292c40164
JasonGlazer <jglazer@gard.com>
2018-06-18 13:26:21 -0500

    Get find_program_by_extension working and use it.

--- eplaunch/utilities/externalprograms.py
+++ eplaunch/utilities/externalprograms.py
@@ -1,16 +1,43 @@
 import subprocess
 import platform
+import wx
 
 
 class EPLaunchExternalPrograms:
 
     def __init__(self):
-        self.text_editor_binary = self.find_program_by_extension("txt")
-        self.pdf_viewer_binary = self.find_program_by_extension("pdf")
-        self.spreadsheet_binary = self.find_program_by_extension("csv")
+        self.text_editor_binary = self.find_program_by_extension(".txt", "")
+        self.pdf_viewer_binary = self.find_program_by_extension(".pdf", self.text_editor_binary)
+        self.spreadsheet_binary = self.find_program_by_extension(".csv", self.text_editor_binary)
+        self.drawing_binary = self.find_program_by_extension(".dxf", self.text_editor_binary)
+        self.vrml_binary = self.find_program_by_extension(".wrl", self.text_editor_binary)
+        self.diagramming_binary = self.find_program_by_extension(".svg", self.text_editor_binary)
+        self.web_browser_binary = self.find_program_by_extension(".html", self.text_editor_binary)
+        self.web_browser_binary = self.find_program_by_extension(".eso", self.text_editor_binary)
+        self.xmlviewer_binary = self.find_program_by_extension(".xml", self.text_editor_binary)
 
-    def find_program_by_extension(self, extension_string):
-        pass
+    def find_program_by_extension(self, extension_string, not_found_application_path):
+        # print(extension_string)
+        # from wxPython Demo for MimeTypesManager
+        ft = wx.TheMimeTypesManager.GetFileTypeFromExtension(extension_string)
+        # print(ft.GetMimeType())
+        extList = ft.GetExtensions()
+        if extList:
+            ext = extList[0]
+            if len(ext) > 0 and ext[0] == ".": ext = ext[1:]
+        else:
+            ext = ""
+        filename = "SPAM" + "." + ext
+        mime = ft.GetMimeType() or ""
+        params = wx.FileType.MessageParameters(filename, mime)
+        cmd = ft.GetOpenCommand(params)
+        if cmd:
+            application_path = cmd.split('"')[1]
+            # print(application_path)
+            return application_path
+        else:
+            # print("No application found")
+            return not_found_application_path
 
     def run_idf_editor(self, file_path):
         # todo: find the idf editor binary automatically based on the workflow
