e22be353826cf8cfff79bc3b24384b68c2c935e3
Edwin Lee <leeed2001@gmail.com>
2018-08-06 21:53:18 -0500

    Clean up commands, requirements

--- setup.py
+++ setup.py
@@ -1,32 +1,7 @@
-# from setuptools import setup
-#
-# setup(
-#     name='EPLaunch3',
-#     version='0.1',
 #     packages=['eplaunch', 'eplaunch.interface'],
-#     url='https://energyplus.net',
-#     license='ModifiedBSD',
-#     author='DOE',
-#     author_email='webmaster@doe.gov',
-#     description='Graphical Interface and Workflow Manager for EnergyPlus',
 #     scripts='eplaunch/runner.py',
 #     test_suite="eplaunch.tests",
-#     options={"py2exe": {"compressed": 2,
-#                         "optimize": 2,
-#                         "includes": [],
-#                         "excludes": [],
-#                         "packages": [],
-#                         "dll_excludes": [],
-#                         "bundle_files": 3,
-#                         "dist_dir": "dist",
-#                         "xref": False,
-#                         "skip_archive": False,
-#                         "ascii": False,
-#                         "custom_boot_script": '',
-#                         }
-#              },
-#     windows=['sampleApp.py']
-# )
+
 
 import glob
 import os
@@ -41,9 +16,9 @@ include_files.extend(workflow_tuples)
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-      "packages": ["eplaunch", "os"],
-      "excludes": ["tkinter"],
-      "include_files": include_files
+    "packages": ["eplaunch", "os"],
+    "excludes": ["tkinter"],
+    "include_files": include_files
 }
 
 # GUI applications require a different base on Windows (the default is for a
@@ -52,8 +27,13 @@ base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-setup(name="EP-Launch",
-      version="0.1",
-      description="My GUI application!",
-      options={"build_exe": build_exe_options},
-      executables=[Executable("eplaunch/runner.py", targetName="EPLaunch", base=base)])
+setup(
+    name="EP-Launch",
+    version="0.1",
+    author='Jason Glazer and Edwin Lee for the United States Department of Energy',
+    description='Graphical Interface and Workflow Manager for EnergyPlus',
+    url='https://github.com/NREL/EP-Launch',
+    license='ModifiedBSD',
+    options={"build_exe": build_exe_options},
+    executables=[Executable("eplaunch/runner.py", targetName="EPLaunch", base=base)]
+)
