95bba280460328674547598d54bf42632502e7dd
Edwin Lee <leeed2001@gmail.com>
2018-08-06 21:17:41 -0500

    Move wxpython install to deploy script

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+pip install --upgrade --pre https://extras.wxpython.org/wxPython4/extras/linux/gtk2/ubuntu-16.04/wxPython-4.0.0b2-cp36-cp36mu-linux_x86_64.whl
 python setup.py build && tar -zcvf eplaunch.tar.gz build/exe.linux-x86_64-3.6
