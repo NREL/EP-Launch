a0bc0ea6c5e6bc1c8ed60856f418b474da391487
Edwin Lee <leeed2001@gmail.com>
2018-08-06 21:21:14 -0500

    Getting closer, changed ubuntu version on wxpython wheel

--- scripts/build_and_deploy.sh
+++ scripts/build_and_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install --upgrade --pre https://extras.wxpython.org/wxPython4/extras/linux/gtk2/ubuntu-16.04/wxPython-4.0.0b2-cp36-cp36mu-linux_x86_64.whl
+pip install --upgrade --pre https://extras.wxpython.org/wxPython4/extras/linux/gtk2/ubuntu-14.04/wxPython-4.0.3-cp36-cp36m-linux_x86_64.whl
 python setup.py build && tar -zcvf eplaunch.tar.gz build/exe.linux-x86_64-3.6
