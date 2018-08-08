694d88ffa187aab5c8a11f761e24cb4fda31ea3f
Edwin Lee <leeed2001@gmail.com>
2018-08-08 09:02:59 -0500

    Fixup artifacting in both Travis and AppVeyor

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -2,3 +2,5 @@
 
 python setup.py build
 tar -zcf EP-Launch-${VERSION}-linux.tar.gz build/exe.linux-x86_64-3.6
+mkdir tmp_build
+cp EP-Launch-${VERSION}-linux.tar.gz tmp_build
--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -3,4 +3,5 @@
 pip install wxpython
 python setup.py bdist_mac
 tar -zcf EP-Launch-${VERSION}-mac.tar.gz build/EP-Launch-0.1.app
-exit 0
+mkdir tmp_build
+cp EP-Launch-${VERSION}-mac.tar.gz tmp_build
