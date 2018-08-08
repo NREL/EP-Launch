d8b80eb7fa84e3f71ae44aab12ac060d348e1660
Edwin Lee <leeed2001@gmail.com>
2018-08-08 09:21:53 -0500

    Automatically get VERSION from eplaunch lib

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
 python setup.py build
 tar -zcf EP-Launch-${VERSION}-linux.tar.gz build/exe.linux-x86_64-3.6
 mkdir tmp_build
--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
 pip install wxpython
 python setup.py bdist_mac
 tar -zcf EP-Launch-${VERSION}-mac.tar.gz build/EP-Launch-0.1.app
