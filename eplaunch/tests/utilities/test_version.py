e105caa85b491a77b2c927e6180b2250ec35d7d1
Edwin Lee <leeed2001@gmail.com>
2018-08-13 19:21:16 -0500

    Skipping file on Windows

--- eplaunch/tests/utilities/test_version.py
+++ eplaunch/tests/utilities/test_version.py
@@ -1,6 +1,7 @@
 import os
 import unittest
 
+from eplaunch.utilities.crossplatform import Platform
 from eplaunch.utilities.version import Version
 
 
@@ -64,6 +65,8 @@ class TestVersion(unittest.TestCase):
         self.assertEqual('', version_string)
         self.assertEqual(0, version_number)
 
+    @unittest.skipUnless(Platform.get_current_platform() == Platform.LINUX,
+                         "Test badly encoded file on Linux systems, test fails on Windows")
     def test_check_energyplus_version_bad_file(self):
         file_path = os.path.join(os.path.dirname(__file__), "Minimal_820.idf")
         is_version_found, version_string, version_number = self.v.check_energyplus_version(file_path)
--- setup.py
+++ setup.py
@@ -5,17 +5,23 @@ import sys
 from cx_Freeze import setup as cx_setup, Executable
 
 from eplaunch import NAME, VERSION
+# from eplaunch.utilities.crossplatform import Platform
 
 include_files = []
 default_workflows = glob.glob("eplaunch/workflows/default/site_location.py")
 workflow_tuples = [(x, os.path.join('lib', x)) for x in default_workflows]
 include_files.extend(workflow_tuples)
 
+include_dependencies = []
+#
+# if Platform.get_current_platform() == Platform.LINUX:
+#     include_dependencies.append(("/usr/lib/libpng12.so.0.37.0", "libpng.so.0"))
+
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    "packages": ["eplaunch", "os"],
+    "packages": ["eplaunch", "os", "wx", "pubsub.pub"],
     "excludes": ["tkinter"],
-    "include_files": include_files
+    "include_files": include_files + include_dependencies
 }
 
 # GUI applications require a different base on Windows (the default is for a
