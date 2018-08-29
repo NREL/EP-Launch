f7038b50db594cdb6e890367c9a2d68af7bb6563
Edwin Lee <leeed2001@gmail.com>
2018-08-29 14:59:26 -0500

    Add debug

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -3,6 +3,7 @@
 VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
 pip install wxpython
 pyinstaller --onefile --windowed eplaunch.spec
+ls dist/*
 tar -zcf EP-Launch-${VERSION}-mac.tar.gz dist/eplaunch.app
 mkdir tmp_build
 cp EP-Launch-${VERSION}-mac.tar.gz tmp_build
