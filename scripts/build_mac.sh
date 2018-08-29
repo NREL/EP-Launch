dbff6bdd989c35110ff815e29e71e84e1c059d79
Edwin Lee <leeed2001@gmail.com>
2018-08-29 13:37:31 -0500

    Try using pyinstaller instead of cx_freeze

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -2,7 +2,7 @@
 
 VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
 pip install wxpython
-python setup.py bdist_mac
-tar -zcf EP-Launch-${VERSION}-mac.tar.gz build/EP-Launch-0.1.app
+pyinstaller eplaunch.spec
+tar -zcf EP-Launch-${VERSION}-mac.tar.gz dist/EPLaunch
 mkdir tmp_build
 cp EP-Launch-${VERSION}-mac.tar.gz tmp_build
