8f2ed11cd00cffb9399cedd1c8183ffb7e68e897
Edwin Lee <leeed2001@gmail.com>
2018-08-29 23:27:44 -0500

    Use 8 characters for SHA

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
-SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-6`
+SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-8`
 pyinstaller eplaunch.spec
 tar -zcf EP-Launch-${VERSION}-${SHA}-linux.tar.gz dist/EPLaunch
 mkdir tmp_build
--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
-SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-6`
+SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-8`
 pip install wxpython
 pyinstaller --onefile --windowed eplaunch.spec
 ls dist/*
