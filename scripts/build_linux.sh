93ce11c45bc4788ed590e7bebc05589e30de4d05
Edwin Lee <leeed2001@gmail.com>
2018-08-29 23:57:09 -0500

    Change tar file roots on Mac/Linux, fix command line flag on NSIS

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -3,6 +3,6 @@
 VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
 SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-8`
 pyinstaller eplaunch.spec
-tar -zcf EP-Launch-${VERSION}-${SHA}-linux.tar.gz dist/EPLaunch
+tar -C dist -zcf EP-Launch-${VERSION}-${SHA}-linux.tar.gz EPLaunch
 mkdir tmp_build
 cp EP-Launch-${VERSION}-${SHA}-linux.tar.gz tmp_build
