d29c19bf7fd81ba3a170f4176191be2ceb88061b
Edwin Lee <leeed2001@gmail.com>
2018-08-29 23:11:44 -0500

    Get SHAs from CI and rename files accordingly

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
+SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-6`
 pyinstaller eplaunch.spec
-tar -zcf EP-Launch-${VERSION}-linux.tar.gz dist/EPLaunch
+tar -zcf EP-Launch-linux-${VERSION}-${SHA}.tar.gz dist/EPLaunch
 mkdir tmp_build
-cp EP-Launch-${VERSION}-linux.tar.gz tmp_build
+cp EP-Launch-linux-${VERSION}-${SHA}.tar.gz tmp_build
--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
+SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-6`
 pip install wxpython
 pyinstaller --onefile --windowed eplaunch.spec
 ls dist/*
-tar -zcf EP-Launch-${VERSION}-mac.tar.gz dist/eplaunch.app
+tar -zcf EP-Launch-mac-${VERSION}-${SHA}.tar.gz dist/eplaunch.app
 mkdir tmp_build
-cp EP-Launch-${VERSION}-mac.tar.gz tmp_build
+cp EP-Launch-mac-${VERSION}-${SHA}.tar.gz tmp_build
