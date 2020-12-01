6d52cf3bdbd84cf445b8f922ac4821e2c6378c3b
Edwin Lee <leeed2001@gmail.com>
2020-11-30 21:23:01 -0600

    First try at release packaging

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-VERSION=`grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2`
-SHA=`echo ${TRAVIS_COMMIT} | cut -c 1-8`
+VERSION=$(grep VERSION eplaunch/__init__.py | cut -d= -f2 | cut -d\" -f2)
+SHA=$(echo "${TRAVIS_COMMIT}" | cut -c 1-8)
+
 pyinstaller eplaunch.spec
-pyinstaller wf_tester.spec
-tar -C dist -zcf EPLaunch-${VERSION}-${SHA}-linux.tar.gz EPLaunch
-tar -C dist -zcf EPLaunchWorkflowTester-${VERSION}-${SHA}-linux.tar.gz EpLaunchWorkflowTester
+tar -C dist -zcf EPLaunch-"${VERSION}"-"${SHA}"-linux.tar.gz EPLaunch
 mkdir tmp_build
-cp EPLaunch-${VERSION}-${SHA}-linux.tar.gz tmp_build
-cp EPLaunchWorkflowTester-${VERSION}-${SHA}-linux.tar.gz tmp_build
+cp EPLaunch-"${VERSION}"-"${SHA}"-linux.tar.gz tmp_build
+
+pyinstaller wf_tester.spec
+tar -C dist -zcf EPLaunchWorkflowTester-"${VERSION}"-"${SHA}"-linux.tar.gz EpLaunchWorkflowTester
+cp EPLaunchWorkflowTester-"${VERSION}"-"${SHA}"-linux.tar.gz tmp_build
