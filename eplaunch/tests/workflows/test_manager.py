75a0b275f2da9984886d98db34f7b81ae380b60f
Edwin Lee <leeed2001@gmail.com>
2021-05-17 10:24:34 -0500

    Continue coverage, I think thats as far as Im going to get

--- eplaunch/tests/workflows/test_manager.py
+++ eplaunch/tests/workflows/test_manager.py
@@ -29,7 +29,7 @@ class SiteLocationWorkflow(BaseEPLaunchWorkflow1):
     def name(self): return 'dummy'
     def context(self): return 'theseWorkflows'
     def description(self): return 'Dummy workflow'
-    def get_file_types(self): return ['*.txt']
+    def get_file_types(self): return ['*.txt', '*.pdf']
     def get_output_suffixes(self): return []
     def get_interface_columns(self): return ['dummy']
     def main(self, run_directory, file_name, args):
@@ -87,6 +87,21 @@ class SiteLocationWorkflow(UnknownWorkflowClass):
         self.assertEqual(len(external_only_workflows), 0)
         self.assertEqual(1, len(warnings))
 
+    def test_incomplete_workflow(self):
+        # note that the workflow does not implement all the abstract methods
+        file_contents = """
+from eplaunch.workflows.base import BaseEPLaunchWorkflow1, EPLaunchWorkflowResponse1
+class SiteLocationWorkflow(BaseEPLaunchWorkflow1):
+    ...
+        """
+        with open(os.path.join(self.extra_workflow_dir, 'incomplete_workflow.py'), 'w') as f:
+            f.write(file_contents)
+        initial_workflows = set()
+        initial_workflows.add(self.extra_workflow_dir)
+        external_only_workflows, warnings = get_workflows(initial_workflows, disable_builtins=True)
+        self.assertEqual(len(external_only_workflows), 0)
+        self.assertEqual(1, len(warnings))
+
     def test_energyplus_workflow(self):
         file_contents = """
 from eplaunch.workflows.base import BaseEPLaunchWorkflow1, EPLaunchWorkflowResponse1
@@ -109,3 +124,25 @@ class SiteLocationWorkflow(BaseEPLaunchWorkflow1):
         external_only_workflows, warnings = get_workflows(initial_workflows, disable_builtins=True)
         self.assertEqual(len(external_only_workflows), 1)
         self.assertEqual(0, len(warnings))
+
+    def test_exception_calling_workflow(self):
+        file_contents = """
+from eplaunch.workflows.base import BaseEPLaunchWorkflow1, EPLaunchWorkflowResponse1
+class SiteLocationWorkflow(BaseEPLaunchWorkflow1):
+    def name(self): return 'dummy'
+    def context(self): raise Exception("WHAT HAPPENED!?")
+    def description(self): return 'Dummy workflow'
+    def get_file_types(self): return ['*.txt']
+    def get_output_suffixes(self): return []
+    def get_interface_columns(self): return ['dummy']
+    def main(self, run_directory, file_name, args):
+        return EPLaunchWorkflowResponse1(success=True, message='Hello', column_data={'dummy': location_name})
+        """
+        with open(os.path.join(self.extra_workflow_dir, 'exception_workflow.py'), 'w') as f:
+            f.write(file_contents)
+        initial_workflows = set()
+        initial_workflows.add(self.extra_workflow_dir)
+        external_only_workflows, warnings = get_workflows(initial_workflows, disable_builtins=True)
+        self.assertEqual(len(external_only_workflows), 0)
+        self.assertEqual(1, len(warnings))
+        self.assertIn('Unexpected error', warnings[0])
