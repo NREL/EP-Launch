e4c68d8e2e6f12d2acf6e3341c47edb0f8db4801
Edwin Lee <leeed2001@gmail.com>
2023-04-11 11:48:50 -0500

    Bump version

--- eplaunch/__init__.py
+++ eplaunch/__init__.py
@@ -3,5 +3,5 @@
 # If you waste time on it, please add it here:
 # Total wasted time: 12 minutes
 NAME = "energyplus_launch"
-VERSION = "3.5.7"
+VERSION = "3.5.8"
 DOCS_URL = "https://ep-launch.readthedocs.io/en/latest/"
