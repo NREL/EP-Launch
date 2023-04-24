cdf594c1b4a69b618ad4cb55444503c80fa939ec
Edwin Lee <leeed2001@gmail.com>
2023-04-24 12:05:52 -0500

    I think something went wrong with the version number being duplicated

--- eplaunch/__init__.py
+++ eplaunch/__init__.py
@@ -3,5 +3,5 @@
 # If you waste time on it, please add it here:
 # Total wasted time: 12 minutes
 NAME = "energyplus_launch"
-VERSION = "3.6.5"
+VERSION = "3.6.6"
 DOCS_URL = "https://ep-launch.readthedocs.io/en/latest/"
